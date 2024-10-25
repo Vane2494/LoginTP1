19a196576ce7820af8ecd593830774e507b92347
Vane2494 <147008595+Vane2494@users.noreply.github.com>
2024-10-24 22:29:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById('usuario').value;
+    const contrasena = document.getElementById('contrasena').value;
+
+    fetch('usuarios.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el archivo JSON');
+            }
+            return response.json();
+        })
+        .then(usuarios => {
+            const usuarioEncontrado = usuarios.find(user => user.usuario === usuario && user.contrasena === contrasena);
+            const mensaje = document.getElementById('mensaje');
+
+            if (usuarioEncontrado) {
+                if (usuarioEncontrado.rol === 'admin') {
+                    // Redirigir a la página de administración
+                    window.location.href = 'admin.html';
+                 } else if (usuarioEncontrado.rol === 'creador') {
+                    // Redirigir a la página de creadores de contenido
+                    window.location.href = 'creador.html';
+                } else {
+                    // Redirigir a la página de usuario estándar
+                    window.location.href = 'landing.html';
+                }
+            } else {
+                mensaje.textContent = 'Usuario o contraseña incorrectos';
+                mensaje.style.color = 'red';
+            }
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la petición Fetch:', error);
+        });
+});
